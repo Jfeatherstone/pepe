94dbad4023870c143ba0a479d30678a5075dc41f
Jack Featherstone <jfeatherstone123@gmail.com>
2024-07-26 10:38:34 +0900

    Fixed bug with loading videos, updated to new version

--- pepe/test/test_conv_tracking.py
+++ pepe/test/test_conv_tracking.py
@@ -16,12 +16,14 @@ def test_convCircle_track():
     for i in range(3):
         image = checkImageType(f'{DATA_DIR}/test_circles_{i+1}.bmp').astype(np.uint8)
         radius = 50
-        tolerance = .10 # 10% tolerance of the radius
+        # This is quite a large tolerance if we were actually doing some analysis, but here
+        # we just want to make sure the algorithm works as usual.
+        tolerance = 10 # pixels 
 
         # Parameters aren't too important
         particleCenters, particleRadii = convCircle(image, radius, negativeHalo=True,
-                                                    intensitySoftmax=1.8, intensitySoftmin=.6, peakDownsample=1,
-                                                    offscreenParticles=True, radiusTolerance=2, minPeakPrevalence=.4)
+                                                    intensitySoftmax=2, intensitySoftmin=.5, peakDownsample=1,
+                                                    offscreenParticles=True, radiusTolerance=None, minPeakPrevalence=.4)
 
         with open(f'{DATA_DIR}/test_circles_{i+1}_centers.npy', 'rb') as f:
             expectedCenters = np.load(f)
@@ -32,5 +34,4 @@ def test_convCircle_track():
 
         difference = np.sqrt(np.sum((particleCenters - expectedCenters[sortOrder])**2, axis=-1))
 
-        percentOfRadius = difference / radius
-        assert not True in [p > tolerance for p in percentOfRadius], f"Particle centers not within {tolerance*100}% of expected value in test image {i+1}."
+        assert not (True in [d > tolerance for d in difference]), f"Particle centers not within {tolerance} pixels of expected value in test image {i+1}.\n\nExpected centers:{expectedCenters[sortOrder]}\n\nFound centers:{particleCenters}"
